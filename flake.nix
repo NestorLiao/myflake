0ec87853175ab496cbeb8646d55ea1d04c8b8041
cowboyliao <2730647052@qq.com>
2024-01-14 21:45:17 -0500

    test

--- flake.nix
+++ flake.nix
@@ -5,53 +5,84 @@
     builders-use-substitutes = true;
     experimental-features = [ "nix-command" "flakes" ];
     substituters = [
-      # "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
       "https://cache.nixos.org/"
-      # "https://anyrun.cachix.org"
     ];
     extra-substituters = [
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      # "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
     ];
   };
   
 
   inputs = {
-    nur.url = github:nix-community/NUR;
     rust-overlay.url = "github:oxalica/rust-overlay";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
         url = "github:nix-community/home-manager";
         inputs.nixpkgs.follows = "nixpkgs";
       };
-    # anyrun = {
-    #   url = "github:Kirottu/anyrun";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-    nixvim={
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     # nix-ld={ 
     #   url = "github:Mic92/nix-ld";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
-     # nix-colors.url = "github:misterio77/nix-colors";
   };
 
-  outputs =inputs@{ nixpkgs, home-manager, nur,nixvim,...  }: let
-    system = "x86_64-linux";
-  in
-  {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      inherit (self) outputs;
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+    in
+    rec {
+      # Your custom packages
+      # Acessible through 'nix build', 'nix shell', etc
+      # packages = forAllSystems (system:
+      #   let pkgs = nixpkgs.legacyPackages.${system};
+      #   in import ./pkgs { inherit pkgs; }
+      # );
+      # Devshell for bootstrapping
+      # Acessible through 'nix develop' or 'nix-shell' (legacy)
+      # devShells = forAllSystems (system:
+      #   let pkgs = nixpkgs.legacyPackages.${system};
+      #   in import ./shell.nix { inherit pkgs; }
+      # );
+
+
+      # Your custom packages and modifications, exported as overlays
+      # overlays = import ./overlays { inherit inputs; };
+      # Reusable nixos modules you might want to export
+      # These are usually stuff you would upstream into nixpkgs
+      # nixosModules = import ./modules/nixos;
+      # Reusable home-manager modules you might want to export
+      # These are usually stuff you would upstream into home-manager
+      # homeManagerModules = import ./modules/home-manager;
+
+
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
+      nixosConfigurations = {
+        # FIXME replace with your hostname
+        nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            # > Our main nixos configuration file <
+            ./nixos/configuration.nix
+          ];
+        };
+      };
+
+
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      # system.packages = [ anyrun.packages.${system}.anyrun ];
       modules = [
         ./configuration.nix
-        nur.nixosModules.nur
         # nix-ld.nixosModules.nix-ld
         home-manager.nixosModules.home-manager
         {
@@ -62,9 +93,6 @@
           };
           home-manager.users.randy= {
             imports=[
-              nixvim.homeManagerModules.nixvim
-              # anyrun.homeManagerModules.default
-              # nix-colors.homeManagerModules.default
               ./script
               ./home.nix
             ];
